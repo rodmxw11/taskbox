13221f66f5064f6f218ee197d9e7e975b65ff226
rmaxwell@us.ibm.com <rmaxwell@us.ibm.com>
2019-10-09 18:21:58 -0400

    Added redux

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Task from './Task'
+import { connect } from 'react-redux'
+import { archiveTask, pinTask } from "../lib/redux";
 
-function TaskList({loading, tasks, onPinTask, onArchiveTask}) {
+export function PureTaskList({loading, tasks, onPinTask, onArchiveTask}) {
     const events = {
         onPinTask,
         onArchiveTask
@@ -31,7 +33,7 @@ function TaskList({loading, tasks, onPinTask, onArchiveTask}) {
         return <div className="list-items">
             <div className="wrapper-message">
                 <span className="icon-check" />
-                <div className="title-message">You hvae no tasks</div>
+                <div className="title-message">You have no tasks</div>
                 <div className="subtitle-message">Sit back and relax</div>
             </div>
         </div>
@@ -49,15 +51,23 @@ function TaskList({loading, tasks, onPinTask, onArchiveTask}) {
     )
 }
 
-TaskList.propTypes = {
+PureTaskList.propTypes = {
     loading: PropTypes.bool,
     tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
     onPinTask: PropTypes.func.isRequired,
     onArchiveTask: PropTypes.func.isRequired
 }
 
-TaskList.defaultProps = {
+PureTaskList.defaultProps = {
     loading: false
 }
 
-export default TaskList
+export default connect(
+    ({tasks}) => ({
+        tasks: tasks.filter(t=> t.state==='TASK_INBOX' || t.state==='TASK_PINNED')
+    }),
+    dispatch => ({
+        onArchiveTask: id => dispatch(archiveTask(id)),
+        pinTask: id => dispatch(pinTask(id))
+    })
+) (PureTaskList)
--- src/components/TaskList.test.js
+++ src/components/TaskList.test.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import TaskList from './TaskList'
+import { PureTaskList } from './TaskList'
 import { withPinnedTasks} from "./TaskList.stories"
 
 it ("renders pinned tasks at the start of the list", () => {
     const div = document.createElement('div')
     const events = { onPinTask: jest.fn(), onArchiveTask: jest.fn()}
 
-    ReactDom.render(<TaskList tasks={withPinnedTasks} {...events} />, div)
+    ReactDom.render(<PureTaskList tasks={withPinnedTasks} {...events} />, div)
 
     // We expect the task titled 'Task 6 (pinned)' to be rendered first, not at the end
     const lastTaskInput = div.querySelector('.list-item:nth-child(1) input[value="Task 6 (pinned)"]')
