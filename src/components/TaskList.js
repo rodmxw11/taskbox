e57c71605c48170d7fffe9c1140680f0a80b4407
rmaxwell@us.ibm.com <rmaxwell@us.ibm.com>
2019-10-09 17:24:22 -0400

    TaskList handles task states

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -7,17 +7,43 @@ function TaskList({loading, tasks, onPinTask, onArchiveTask}) {
         onArchiveTask
     }
 
+    const LoadingRow = (
+        <div className="loading-item">
+            <span className="glow-checkbox" />
+            <span className="glow-text">
+                <span>Loading</span> <span>cool</span> <span>state</span>
+            </span>
+        </div>
+    )
     if (loading) {
-        return <div className="list-itmes">Loading</div>
+        return <div className="list-itmes">
+            {LoadingRow}
+            {LoadingRow}
+            {LoadingRow}
+            {LoadingRow}
+            {LoadingRow}
+            {LoadingRow}
+        </div>
     }
 
     if (tasks.length===0) {
-        return <div className="list-items">Empty</div>
+        return <div className="list-items">
+            <div className="wrapper-message">
+                <span className="icon-check" />
+                <div className="title-message">You hvae no tasks</div>
+                <div className="subtitle-message">Sit back and relax</div>
+            </div>
+        </div>
     }
+
+    const tasksInOrder = [
+        ...tasks.filter( t => t.state==='TASK_PINNED'),
+        ...tasks.filter( t => t.state!=='TASK_PINNED')
+    ]
 
     return (
         <div className="list-items">
-            {tasks.map( task => <Task key={task.id} task={task} {...events} />)}
+            {tasksInOrder.map( task => <Task key={task.id.toString()} task={task} {...events} />)}
         </div>
     )
 }
